3a904391e8c350aec6e582852526a29059cd75b9
Neo34 <tron31.cc@gmail.com>
2026-03-19 18:24:59 -0300

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/cesarconstanzo/kanban_board_api/KanbanBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.cesarconstanzo.kanban_board_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanBoardApiApplication.class, args);
+	}
+
+}
