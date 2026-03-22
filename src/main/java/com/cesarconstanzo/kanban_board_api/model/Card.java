da77d616c25deb15fd440882cd9d3a6ab6d9d489
Neo34 <tron31.cc@gmail.com>
2026-03-22 13:10:03 -0300

    feat: add Card entity with JPA annotations

---
+++ src/main/java/com/cesarconstanzo/kanban_board_api/model/Card.java
@@ -0,0 +1,26 @@
+package com.cesarconstanzo.kanban_board_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cards")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean blocked = false;
+}
